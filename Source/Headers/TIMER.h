2ede07a164332d885af3133ff995a9eae8f6b27a
edesvin <edesvin@hotmail.com>
2016-01-07 15:29:19 -0600

    Creations of the following code files: TIMER.h and TIMER.c

---
+++ Source/Headers/TIMER.h
@@ -0,0 +1,56 @@
+/*============================================================================*/
+/*                        I BS SOFTWARE GROUP                                 */
+/*============================================================================*/
+/*                        OBJECT SPECIFICATION                                */
+/*============================================================================*/
+/*!
+ * $Source: TIMER.h $
+ * $Revision: 1.0 $
+ * $Author: 	Edgar Escayola Vinagre	$
+ * 				Adrian Zacarias Siete 	$
+ *				
+ * $Date: 07-01-2016 $
+ */
+/*============================================================================*/
+/* DESCRIPTION :                                                              */
+/** \file
+*	This file contains the configurations that must be done to achieve the 
+*	periodic interrupt that gives the Ticks to the scheduler.
+*/
+/*============================================================================*/
+/* COPYRIGHT (C) CONTINENTAL AUTOMOTIVE 2014                                  */
+/* AUTOMOTIVE GROUP, Interior Division, Body and Security                     */
+/* ALL RIGHTS RESERVED                                                        */
+/*                                                                            */
+/* The reproduction, transmission, or use of this document or its content is  */
+/* not permitted without express written authority. Offenders will be liable  */
+/* for damages.                                                               */
+/* All rights, including rights created by patent grant or registration of a  */
+/* utility model or design, are reserved.                                     */
+/*                                                                            */
+/*============================================================================*/
+/*============================================================================*/
+/*                    REUSE HISTORY - taken over from                         */
+/*============================================================================*/
+/*  DATABASE           |        PROJECT     | FILE VERSION (AND INSTANCE)     */
+/*----------------------------------------------------------------------------*/
+/*                     |      Cluster_EA    |           1.0                   */
+/*============================================================================*/
+/*                               OBJECT HISTORY                               */
+/*============================================================================*/
+/*
+ * $Log: TIMER.h  $
+  ============================================================================*/
+#ifndef TIMER_H_
+#define TIMER_H_
+/* Includes */
+/*============================================================================*/
+#include "IntcInterrupts.h"
+#include "MPC5606B.h"
+#include "Kernel.h"
+
+/* Public functions */
+/*============================================================================*/
+void InitPIT (void);
+void InitSTM (void);
+#endif /* TIMER_H_ */
